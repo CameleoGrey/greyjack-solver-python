f8d9b6ee688c15c448808b6ac21bdf3467ce5646
CameleoGrey <cameleogrey@yandex.ru>
2025-03-02 20:20:15 +0300

    v0.0.4 Planning variables, normal imports from Python (it's very hard to understand, how to start to work with pyo3 and organize the project)

--- examples/check_install/check_install.py
+++ examples/check_install/check_install.py
@@ -1,10 +1,22 @@
 
-import greyjack
+
+#from greyjack.greyjack import GJPlanningVariable
+from greyjack.greyjack import GJPlanningVariable
+from greyjack.variables import GJFloat, GJInteger
 from greyjack.score_requesters.OOPScoreRequester import OOPScoreRequester
 
 if __name__ == "__main__":
 
-    score_requester = OOPScoreRequester()
+    common_variable = GJPlanningVariable(name="x", lower_bound=0.0, upper_bound=1.0, is_int=False, frozen=False, initial_value=None, semantic_groups=None)
+    print(common_variable.fix(-1))
+
+    float_variable = GJFloat(name="x", lower_bound=10.0, upper_bound=100.0, frozen=False, initial_value=None, semantic_groups=None)
+    print(float_variable.fix(1))
+
+    float_variable = GJInteger(name="x", lower_bound=100.0, upper_bound=1000.0, frozen=False, initial_value=None, semantic_groups=None)
+    print(float_variable.fix(10))
+
+    print(OOPScoreRequester())
 
     print("done")
     
