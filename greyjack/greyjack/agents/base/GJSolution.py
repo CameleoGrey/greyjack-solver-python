v0.2.1 Rounding bug fix; incremental score calculation for MathModel (currently works slow, turned off by default, will try to improve later)

greyjack/greyjack/agents/base/GJSolution.py
@@ -6,7 +6,7 @@ class GJSolution():
 
         if discrete_ids is not None:
             for discrete_id in discrete_ids:
-                variable_values[discrete_id] = int(variable_values[discrete_id])
+                variable_values[discrete_id] = int(np.rint(variable_values[discrete_id]))
         
         if score_precision is not None:
             for i in range(len(score_list)):

greyjack/greyjack/pure_math/MathModel.py
@@ -1,6 +1,8 @@
 
 from pprint import pprint
 from greyjack.score_calculation.scores.ScoreVariants import ScoreVariants
+import random
+import numpy as np
 
 class MathModel():
 
@@ -16,6 +18,7 @@ class MathModel():
         self.score_variant = ScoreVariants.HardSoftScore
         self.score_type = None
         self.score_calculator = ScoreCalculatorStub()
+        self.variable_to_constraint_index = None
 
     def get_individual_hard_scores(self, absolute):
 
@@ -64,7 +67,48 @@ class MathModel():
 
         return sum_soft_score
     
-    #def update_model_by_solution(self, gj_solution):
+    def _build_variable_to_constraint_index(self, variables_manager, var_name_to_arr_id_map, discrete_var_names):
+
+        min_trials_count = 100
+        variable_to_constraint_index = {}
+        for var_name in self.variables.keys():
+            
+            variable_to_constraint_index[var_name] = set()
+
+            value_before = self.variables[var_name]
+            already_checked_values = set()
+            current_trials_count = 0
+            different_value_checked = False
+            while current_trials_count < min_trials_count or not different_value_checked:
+                
+                value_after = variables_manager.get_column_random_value(var_name_to_arr_id_map[var_name])
+                if var_name in discrete_var_names:
+                    value_after = int(np.rint(value_after))
+                if value_after == value_before:
+                    continue
+                else:
+                    different_value_checked = True
+
+                if value_after in already_checked_values:
+                    current_trials_count += 1
+                    continue
+                else:
+                    already_checked_values.add(value_after)
+
+                for constraint_name in self.constraints.keys():
+                    penalty_before = self.constraints[constraint_name].get_hard_score(self.variables, self.utility)
+                    self.variables[var_name] = value_after
+                    penalty_after = self.constraints[constraint_name].get_hard_score(self.variables, self.utility)
+                    self.variables[var_name] = value_before
+
+                    if abs(penalty_before) - abs(penalty_after) != 0.0:
+                        variable_to_constraint_index[var_name].add(constraint_name)
+
+                current_trials_count += 1
+
+        return variable_to_constraint_index
+
+
 
 
     def explain_solution(self, gj_solution):
@@ -93,6 +137,8 @@ class MathModel():
         print()
 
         pass
+
+
 
 # filthy way to not rewrite core OOP Agent code
 class ScoreCalculatorStub():

greyjack/greyjack/score_calculation/score_requesters/PureMathScoreRequester.py
@@ -9,6 +9,7 @@ from greyjack.pure_math.variables.BinaryVar import BinaryVar
 from greyjack.variables.GJFloat import GJFloat
 from greyjack.variables.GJInteger import GJInteger
 from greyjack.variables.GJBinary import GJBinary
+import numpy as np
 
 class PureMathScoreRequester():
 
@@ -44,7 +45,11 @@ class PureMathScoreRequester():
             variables_list.append( current_variable )
         self.variables_manager = VariablesManagerPy(variables_list)
         self.n_variables = len(variables_list)
-        self.discrte_ids = self.variables_manager.discrete_ids
+        self.discrete_ids = self.variables_manager.discrete_ids
+        self.discrete_var_names = set()
+        if self.discrete_ids is not None:
+            for discrete_id in self.discrete_ids:
+                self.discrete_var_names.add(self.arr_id_to_var_name_map[discrete_id])
 
     def request_score_plain(self, samples):
 
@@ -52,9 +57,9 @@ class PureMathScoreRequester():
 
         for sample in samples:
 
-            if self.discrte_ids is not None:
-                for i in self.discrte_ids:
-                    sample[i] = int(sample[i])
+            if self.discrete_ids is not None:
+                for i in self.discrete_ids:
+                    sample[i] = int(np.rint(sample[i]))
 
             for i in range(self.n_variables):
                 self.math_model.variables[self.arr_id_to_var_name_map[i]] = sample[i]
@@ -68,4 +73,47 @@ class PureMathScoreRequester():
         return score_batch
     
     def request_score_incremental(self, sample, deltas):
-        raise Exception("request_score_incremental() not implemented for PureMathScoreRequester")
+
+        score_batch = []
+
+        if self.discrete_ids is not None:
+            for i in self.discrete_ids:
+                sample[i] = int(np.rint(sample[i]))
+        for i in range(self.n_variables):
+            self.math_model.variables[self.arr_id_to_var_name_map[i]] = sample[i]
+        
+        if self.math_model.variable_to_constraint_index is None:
+            self.math_model.variable_to_contstraint_index = self.math_model._build_variable_to_constraint_index(self.variables_manager, self.var_name_to_arr_id_map, self.discrete_var_names)
+
+        hard_score_values_before = self.math_model.get_individual_hard_scores(absolute=True)
+        sum_hard_score_before = sum(hard_score_values_before.values())
+        #soft_score_value_before = self.math_model.get_sum_soft_score(is_fitting=True)
+
+        score_batch = []
+        for sample_deltas in deltas:
+            var_names = []
+            values_before = []
+            touched_constraints = []
+            for (i, new_value) in sample_deltas:
+                var_name = self.arr_id_to_var_name_map[i]
+                var_names.append(var_name)
+                values_before.append( self.math_model.variables[var_name] )
+                self.math_model.variables[var_name] = new_value
+                touched_constraints.append(self.math_model.variable_to_contstraint_index[var_name])
+            touched_constraints = set.union(*touched_constraints)
+            
+            sample_hard_score = sum_hard_score_before
+            for constraint_name in touched_constraints:
+                sample_hard_score -= hard_score_values_before[constraint_name]
+                sample_hard_score += abs(self.math_model.constraints[constraint_name].get_hard_score(self.math_model.variables, self.math_model.utility))
+            sample_soft_score = self.math_model.get_sum_soft_score(is_fitting=True)
+
+            score_batch.append(HardSoftScore(sample_hard_score, sample_soft_score))
+
+            for i in range(len(var_names)):
+                self.math_model.variables[var_names[i]] = values_before[i]
+        
+        return score_batch
+            
+
+